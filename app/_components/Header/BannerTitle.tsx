89947091f0e8ed8c2f1d5482dfe2a751a375f3bd
Travis Stanger <61566213+Steezzee@users.noreply.github.com>
2025-10-23 08:28:00 -0500

    Update BannerTitle.tsx
    Corrected Title on Header

--- app/_components/Header/BannerTitle.tsx
+++ app/_components/Header/BannerTitle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BannerTitle: React.FC = () => (
   <div style={{ textAlign: "center", fontSize: "1.8rem", fontWeight: 600 }}>
-    DALLAS HEALTHCARE
+    My Dallas Health
   </div>
 );
 
