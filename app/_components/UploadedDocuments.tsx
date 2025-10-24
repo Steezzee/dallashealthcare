bdf8a3657f80f05dbfab7e5a7697e2135c56d615
Travis Stanger <61566213+Steezzee@users.noreply.github.com>
2025-10-24 15:35:19 -0500

    Update UploadedDocuments.tsx
    Changes to hardcoded data

--- app/_components/UploadedDocuments.tsx
+++ app/_components/UploadedDocuments.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import styles from "./UploadedDocuments.module.css";
 
 const docs = [
-    { name: "Debra Smith", caseNum: "1433232", date: "12/25/25"},
-    { name: "Pablo Sangrito", caseNum: "487240", date: "11/22/25"},
-    { name: "Chico Mojito", caseNum: "388922", date: "1/25/25"},
+    { name: "Debra_Smith_BCBS_051.pdf", caseNum: "1433232", date: "12/25/25"},
+    { name: "Debra_Smith_BCBS_223", caseNum: "4872402", date: "11/22/25"},
+    { name: "Debra_Smith_BCBS_107", caseNum: "3889223", date: "1/25/25"},
 ];
 
 const UploadedDocuments = () => {
