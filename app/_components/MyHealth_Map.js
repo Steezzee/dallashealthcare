a08d52bef829418321d935e3b45e435afd2191c1
munychan2004 <munychan2004@gmail.com>
2025-10-29 15:13:48 -0500

    Fixed the marker not showing and populated sample
    Will likely change sample hospitals until later

--- app/_components/MyHealth_Map.js
+++ app/_components/MyHealth_Map.js
@@ -1,7 +1,26 @@
 'use client';
 
+import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
+
+const localLocations = [
+    {
+        text: "UTD Hypothetical Health Center", 
+        position: [32.9859, -96.7503]
+    },
+    {
+        text: "Another Hypothetical Health Center",
+        position: [32.859, -96.8503]
+    },
+    {
+        text: "Lakewood Neighborhood Medical Clinic",
+        position: [33, -96.85]
+    },
+    {
+        text: "Trinity Express Urgent Care",
+        position: [32.9910, -96.8400]
+    }
+];
 
 function MyHealth_Map() {
     return (
@@ -10,9 +29,11 @@ function MyHealth_Map() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[32.9859, -96.7503]}>
-                <Popup>Fake location</Popup>
-            </Marker>
+            {localLocations.map((loc, idx) => (
+        <Marker key={idx} position={loc.position}>
+          <Popup>{loc.text}</Popup>
+        </Marker>
+            ))}
         </MapContainer>
     )
 }
