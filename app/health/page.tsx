0bb477a884e5338b0e7ae95af778da07cf4c1d72
munychan2004 <munychan2004@gmail.com>
2025-11-12 16:28:35 -0600

    fixed hydration error with extraneous use client

--- app/health/page.tsx
+++ app/health/page.tsx
@@ -1,9 +1,11 @@
-import HealthPageClient from "./HealthPageClient";
+//import HealthPageClient from "./HealthPageClient";
 import AppointmentObject from "../_components/AppointmentTracker/AppointmentObject";
 
-/* this page could theoretically be just be HealthPageClient but there is apparently an issue between parent and 
-and functions  returning AppointmentObjects fully functions but HealthPageClient itself had issues with addAppointment being recognized
-I still kinda don't understand it fully even after looking it up for half an hour  */
+/* page could theoretically be just be HealthPageClient but there is apparently an issue between parent and 
+and functions
+returning AppointmentObjects fully functions but HealthPageClient itself had issues with addAppointment being recognized
+I still kinda don't understand it fully even after looking it up for half an hour.  
+Doesn't need fixing right now since we aren't dealig with backend IMO  */
 export default function Page() {
   return <AppointmentObject/>;
 }
