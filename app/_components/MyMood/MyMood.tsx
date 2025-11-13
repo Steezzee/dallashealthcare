6568eef2c466bf983b856d093d6c87b9f145d5bd
ange-222 <angeline.pereiraa@gmail.com>
2025-11-13 16:54:17 -0600

    removed emojis bc not functional

--- app/_components/MyMood/MyMood.tsx
+++ app/_components/MyMood/MyMood.tsx
@@ -1,25 +1,13 @@
 import React from "react";
 import styles from "./MyMood.module.css";
 
-
+{/* hardcoded entry spot for potential mood entries for users */}
 const MyMood: React.FC = () => (
     <div className={styles.container}>
         <h2 className={styles.heading}> My Mood  </h2> 
-        
-        {/* emojis for mood identification */}
-        <div className={styles.moodSelector}>
-            {['😊', '😐', '😔', '😠', '😴', '😡'].map((emoji) => (
-                <button key={emoji} className={styles.emojiButton}>
-                    {emoji}
-                </button>
-            ))}
-        </div>
-        
-        {/*field ask user how they are */}
-        <textarea
-            className={styles.entry}
-            placeholder="How are you feeling today?"
-            />
+
+
+        <textarea className={styles.entry} placeholder="How are you feeling today?"/>
     </div>
 );
 
