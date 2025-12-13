929811011593d475b966b7c7c8bc9d117d084fba
Bitania Kebede <bitulucy@gmail.com>
2025-12-12 20:00:52 -0600

    Hover tool

--- app/_components/TabBar/TabBar.tsx
+++ app/_components/TabBar/TabBar.tsx
@@ -6,10 +6,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const tabs = [
-    { label: "MyHomepage", href: "/home", color: "#0E3C35", fontFamily: "Kanit, sans-serif" },
-    { label: "MyInsurance", href: "/insurance", color: "#0E3C35", fontFamily: "Kanit, sans-serif" },
-    { label: "MyHealth", href: "/health", color: "#0E3C35", fontFamily: "Kanit, sans-serif" },
-    { label: "MyCost", href: "/cost", color: "#0E3C35", fontFamily: "Kanit, sans-serif" },
+    { label: "MyHomepage", href: "/home", color: "#0E3C35", fontFamily: "Kanit, sans-serif", title: "View appointments, prescriptions, and doctor's notes.",},
+    { label: "MyInsurance", href: "/insurance", color: "#0E3C35", fontFamily: "Kanit, sans-serif", title: "View and upload insurance documents and active insurances.",},
+    { label: "MyHealth", href: "/health", color: "#0E3C35", fontFamily: "Kanit, sans-serif", title: "Find a health provider and book an appointment.",},
+    { label: "MyCost", href: "/cost", color: "#0E3C35", fontFamily: "Kanit, sans-serif", title: "Calculate potential estimate costs of procedures.",},
 ];
 
 const TabBar: React.FC = () => {
@@ -18,14 +18,20 @@ const TabBar: React.FC = () => {
 
     return (
         <nav className={styles.tabBar}>
-           {tabs.map(({ label, href }) => {
+           {tabs.map(({ label, href, title }) => {
             const isActive = pathname === href;
-            return (
-                <Link key={label} href={href} className={`${styles.tabButton} ${isActive ? styles.activeTab : ""}`}>
+                return (
+                    <Link
+                    key={label}
+                    href={href}
+                    title={title}
+                    className={`${styles.tabButton} ${isActive ? styles.activeTab : ""}`}
+                    >
                     {label}
-                </Link>
-            );
-           })}
+                    </Link>
+                );
+            })}
+
         </nav>
     );
 };
